0a8926ba31bf09037b6e79b709402e925af6ca0c
UNDERDOG2301 <andersonpalominosandoval@gmail.com>
2024-09-14 19:33:18 -0500

    ultimos detalles

--- js/app.js
+++ js/app.js
@@ -77,13 +77,23 @@ const crearIngresoHTML = (ingreso) => {
                         )}</div>
                         <div class="elemento_eliminar">
                             <button class="elemento_eliminar--btn">
-                                <ion-icon name="close-circle-outline"></ion-icon>
+                                <ion-icon name="close-circle-outline"
+                                onclick="eliminarIngreso(${
+                                  ingreso.id
+                                })"></ion-icon>
                             </button>
                         </div>
                     </div>
                 </div>
     `;
   return ingresoHTML;
+};
+
+const eliminarIngreso = (id) => {
+  let indiceEliminar = ingresos.findIndex((ingreso) => ingreso.id == id);
+  ingresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarIngresos();
 };
 
 const cargarEgresos = () => {
@@ -109,11 +119,39 @@ const crearEgresoHTML = (egreso) => {
                         )}</div>
                         <div class="elemento_eliminar">
                             <button class="elemento_eliminar--btn">
-                                <ion-icon name="close-circle-outline"></ion-icon>
+                                <ion-icon name="close-circle-outline"
+                                onclick="eliminarEgreso(${
+                                  egreso.id
+                                })"></ion-icon>
                             </button>
                         </div>
                     </div>
                 </div>
     `;
   return egresoHTML;
+};
+
+let eliminarEgreso = (id) => {
+  let indiceEliminar = egresos.findIndex((egreso) => egreso.id == id);
+  egresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarEgresos();
+};
+
+let agregarDato = () => {
+  let forma = document.forms["forma"];
+  let tipo = forma["tipo"];
+  let descripcion = forma["descripcion"];
+  let valor = forma["valor"];
+  if (descripcion.value !== "" && valor.value !== "") {
+    if (tipo.value === "ingreso") {
+      ingresos.push(new Ingreso(descripcion.value, +valor.value));
+      cargarCabecero();
+      cargarIngresos();
+    } else if (tipo.value === "egreso") {
+      egresos.push(new Egreso(descripcion.value, +valor.value));
+      cargarCabecero();
+      cargarEgresos();
+    }
+  }
 };
